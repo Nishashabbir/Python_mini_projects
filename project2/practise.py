c9fd9a9c76fc18026035289c83fa05c081caa1a0
Nisha <nishashabbiranjum@gmail.com>
2026-05-10 01:27:30 +0500

    I Yearn for it , I learn for it

--- project2/practise.py
+++ project2/practise.py
@@ -2,67 +2,67 @@
 
 
 
-# import tkinter as tk
-# from tkinter import messagebox
+import tkinter as tk
+from tkinter import messagebox
 
 
-# window=tk.Tk() #window in which the grid for game is being created 
-# window.title("tick tack toe ")
+window=tk.Tk() #window in which the grid for game is being created 
+window.title("tick tack toe ")
 
-# board=[""] *9 #these are the solts 
-# buttons=[]  #we will append the buttons UI in it 
-# current_player="X"
+board=[""] *9 #these are the solts 
+buttons=[]  #we will append the buttons UI in it 
+current_player="X"
 
 
-# def checkWinner():
-#     winning=[(0,1,2) , (3,4,5) , (6,7,8) , (0,3,6) , (1,4,7) , (2,5,8),(0,4,8) , (2,4,6)]
+def checkWinner():
+    winning=[(0,1,2) , (3,4,5) , (6,7,8) , (0,3,6) , (1,4,7) , (2,5,8),(0,4,8) , (2,4,6)]
 
-#     for a , b , c in winning:
-#         if board[a]==board[b]==board[c]!="":
-#             return board[a] #all are same , either X will be there or O
-#     return None 
+    for a , b , c in winning:
+        if board[a]==board[b]==board[c]!="":
+            return board[a] #all are same , either X will be there or O
+    return None 
     
-# def click(index): #at every click , check if slot is empty then add a player move , 
-#     global current_player
-#     if board[index]== "":
-#         board[index]=current_player
-#         buttons[index].config(text=current_player) #for Text UI
+def click(index): #at every click , check if slot is empty then add a player move , 
+    global current_player
+    if board[index]== "":
+        board[index]=current_player
+        buttons[index].config(text=current_player) #for Text UI
 
-#         winner =checkWinner() #keeps checking the wins
+        winner =checkWinner() #keeps checking the wins
 
-#         if winner:
-#             messagebox.showinfo("Game Over" , f"the player {current_player} wins!")
-#             resetgame()
-#             return
-#         #if board!= "": #this is wrong line , it means when the board is not empty which can happen at the very first click when one slot is allocated only 
-#         if  "" not in board : #now this means when not even one empty seat left in the board 
-#             messagebox.showinfo("The Game is Over" , f"the match is draw! ")
-#             resetgame()
-#             return
+        if winner:
+            messagebox.showinfo("Game Over" , f"the player {current_player} wins!")
+            resetgame()
+            return
+        #if board!= "": #this is wrong line , it means when the board is not empty which can happen at the very first click when one slot is allocated only 
+        if  "" not in board : #now this means when not even one empty seat left in the board 
+            messagebox.showinfo("The Game is Over" , f"the match is draw! ")
+            resetgame()
+            return
 
-#         current_player="O" if current_player=="X" else "X" #switch the existing player everytime before the next click 
+        current_player="O" if current_player=="X" else "X" #switch the existing player everytime before the next click 
 
-# def resetgame():
-#     global buttons , board , current_player #changing the same variables 
-#     board=[""]*9
-#     current_player="X"
-#     for btn in buttons: #this is for UI so remove the text on button  remvoe X or O
-#         btn.config(text="") #removing text for next game
+def resetgame():
+    global buttons , board , current_player #changing the same variables 
+    board=[""]*9
+    current_player="X"
+    for btn in buttons: #this is for UI so remove the text on button  remvoe X or O
+        btn.config(text="") #removing text for next game
 
-# # now actually creating the buttons 
-# for i in range(9): #creating 9 required buttons
-#     btn=tk.Button(
-#         window ,
-#         text="" ,
-#         width=5,
-#         height=2,
-#         font=("Arial" , 24),
-#         command= lambda i=i: click(i)
-#         )
-#     btn.grid(row= i//3 , column= i%3) #intrsting way to change UI through maths 
-#     buttons.append(btn)
-# window.mainloop() #finally running the app created in window 
-##and the app never stops running becuase of this line as well 
+# now actually creating the buttons 
+for i in range(9): #creating 9 required buttons
+    btn=tk.Button(
+        window ,
+        text="" ,
+        width=5,
+        height=2,
+        font=("Arial" , 24),
+        command= lambda i=i: click(i)
+        )
+    btn.grid(row= i//3 , column= i%3) #intrsting way to change UI through maths 
+    buttons.append(btn)
+window.mainloop() #finally running the app created in window 
+#and the app never stops running becuase of this line as well 
 
 
 # lambda working   : lamda is just a function without a name and it can take any number of arguments but can only have one expression
---
+++ project3/Digital_clock.py
@@ -0,0 +1,61 @@
+
+# we are going to make it user friendly through GUI ,  using tkiter library 
+
+import tkinter as tk
+from time import strftime
+
+window =tk.Tk()
+window.title("Digital Clock")
+
+def time():
+    string= strftime('%H:%M:%S %p \n %D')
+    label.config(text=string)
+    label.after(1000, time)
+# label is just being created but to visualize it we will use pack 
+label= tk.Label(window, font=('calibari' , 50 , 'bold') , background="Green" , foreground="white")
+label.pack(anchor='center') #here we used pack becuase to visually show the label that we created 
+
+time()
+window.mainloop()
+    
+
+
+
+# practise again 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import tkinter as tk
+from time import strftime
+
+window=tk.Tk()
+window.title("Digital Clock")
+
+def time():
+    string=strftime('%H:%M:%S \n %D')
+    label.config(text=string)
+    label.after(1000, time)
+
+label=tk.Label(window , font=('calibari' , 50 , "bold" ) , background="yellow" , foreground="black")
+label.pack(anchor="center")
+
+
+
+
+
+
+time()
+window.mainloop()
